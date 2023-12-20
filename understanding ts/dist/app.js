583c3f49089fcf4cef7e0dfbe967e57c3c4e2f7a
jeyaprakash <jeyaprakash.jsx@gmail.com>
2023-12-20 13:15:00 +0530

    classes

--- understanding ts/dist/app.js
+++ understanding ts/dist/app.js
@@ -1,9 +1,73 @@
 "use strict";
-console.log('This is app.ts file ');
-let userName = "app.js";
-const button = document.querySelector('button');
-function clickHandler(message) {
-    console.log('This is message : ', message);
+class Department {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this.employee = [];
+    }
+    static createEmployee(name) {
+        console.log(name);
+    }
+    describe() {
+        console.log(`Department id ${this.id} name : ${this.name}`);
+    }
+    addEmployee(employ) {
+        this.employee.push(employ);
+    }
+    printEmployee() {
+        console.log(this.employee);
+    }
 }
-button === null || button === void 0 ? void 0 : button.addEventListener('click', clickHandler.bind(null, 'you are welcome'));
+Department.fiscalYear = 2024;
+class ITDepartment extends Department {
+    constructor(id, admin) {
+        super(id, 'IT');
+        this.admins = admin;
+    }
+    addAdmin(admin) {
+        this.admins.push(admin);
+    }
+    printAdmin() {
+        console.log('Admins : ', this.admins);
+    }
+}
+class TransportDepartment extends Department {
+    get mostRecentReport() {
+        if (this.last_reports)
+            return this.last_reports;
+    }
+    constructor(id, report) {
+        super(id, 'Transport');
+        this.reports = report;
+        this.last_reports = report[0];
+    }
+    set mostRecentReport(value) {
+        if (!value) {
+            throw new Error('Please enter valid input');
+        }
+        this.addReport(value);
+    }
+    printReport() {
+        console.log('Reports : ', this.reports);
+    }
+    addReport(text) {
+        this.reports.push(text);
+        this.last_reports = text;
+    }
+}
+Department.createEmployee('our new employeed');
+console.log('fiscal year : ', Department.fiscalYear);
+const IT = new ITDepartment("D1", []);
+IT.describe();
+IT.addAdmin('jp');
+IT.addEmployee('jp');
+IT.addEmployee('subha');
+IT.printEmployee();
+IT.printAdmin();
+const transport = new TransportDepartment("D2", []);
+transport.mostRecentReport = "This is year end report";
+transport.addReport('Something went wront');
+transport.printReport();
+console.log('most recent report ', transport.mostRecentReport);
+console.log(IT);
 //# sourceMappingURL=app.js.map
--- understanding ts/src/app.ts
+++ understanding ts/src/app.ts
@@ -1,16 +1,110 @@
-console.log('This is app.ts file ')
-let userName = "app.js"
+class Department{
+    //readonly id:string;
+    //name:string;
 
-const button = document.querySelector('button');
+    static fiscalYear:number = 2024;
+    private employee:string[] = [];
 
+    constructor(private readonly id:string,public name:string){
+        //this.id = id;
+        //this.name = name;
+    }
 
-function clickHandler(message:string){
-    // let s;
-    console.log('This is message : ',message);
+    static createEmployee(name:string){
+        console.log(name);
+    }
+
+    describe(this:Department){
+        console.log(`Department id ${this.id} name : ${this.name}`)
+    }
+
+    addEmployee(employ:string){
+        this.employee.push(employ)
+    }
+
+    printEmployee(){
+        console.log(this.employee)
+    }
+
 }
 
-//comment added
-button?.addEventListener('click',clickHandler.bind(null,'you are welcome'))
+
+class ITDepartment extends Department {
+    admins:string[];
+
+    constructor(id:string,admin:string[]){
+        super(id,'IT')
+        this.admins = admin;
+    }
+
+    addAdmin(admin:string){
+        this.admins.push(admin)
+    }
+
+    printAdmin(){
+        console.log('Admins : ',this.admins)
+    }
+
+}
+
+
+class TransportDepartment extends Department {
+    reports:string[];
+    private last_reports:string;
+
+    get mostRecentReport() {
+        if(this.last_reports)
+        return this.last_reports;
+    }
+    constructor(id:string,report:string[]){
+        super(id,'Transport')
+        this.reports = report;
+        this.last_reports = report[0];
+    }
+
+    set mostRecentReport(value){
+        if(!value){
+            throw new Error('Please enter valid input')
+        }
+        this.addReport(value)
+    }
+
+    printReport(){
+        console.log('Reports : ',this.reports)
+    }
+
+    addReport(text:string){
+        this.reports.push(text);
+        this.last_reports = text;
+    }
+
+}
+
+Department.createEmployee('our new employeed');
+console.log('fiscal year : ',Department.fiscalYear)
+const IT = new ITDepartment("D1",[])
+IT.describe();
+
+IT.addAdmin('jp')
+IT.addEmployee('jp');
+IT.addEmployee('subha')
+
+IT.printEmployee();
+IT.printAdmin();
+
+
+const transport = new TransportDepartment("D2",[])
+transport.mostRecentReport="This is year end report"
+transport.addReport('Something went wront')
+
+transport.printReport();
+ 
+console.log('most recent report ',transport.mostRecentReport)
+
+// const accCopy = {name:'dummy',describe:accounting.describe}
+
+// accCopy.describe();
+ console.log(IT)
 
 
 
